98282628ae7070cc77214f26588ff4228d43c0ef
unknown <ahmednahid1995@gmail.com>
2022-03-26 01:59:48 +0600

    header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Batsman Empire</h1>
+            <h2>Pick fantastic 4 bats</h2>
+        </div>
+    );
+};
+
+export default Header;
